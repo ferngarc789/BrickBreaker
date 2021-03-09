97e48d6aa6dc3300eceaacfa8e3e34dbc87127b9
Fernando Garcia <ferngarc@live.unc.edu>
2021-03-09 10:44:08 -0500

    Initial commit

---
+++ src/GamePlay.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GamePlay extends JPanel implements KeyListener, ActionListener {
+    private boolean play = false;
+    private int score = 0;
+    private int totalBricks = 21;
+    private Timer timer;
+    private int delay = 8;
+    private int playerX = 310;
+    private int ballposX = 120;
+    private int ballposy = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+
+    public GamePlay() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+    public void paint(Graphics g) {
+        // this will be the background
+        g.setColor(Color.black);
+        g.fillRect(1, 1, 692, 592);
+
+        //the border
+        g.setColor(Color.YELLOW);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        //the paddle
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+
+        //ball
+        g.setColor(Color.yellow);
+        g.fillRect(ballposX, ballposy, 20, 20);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode()) ==
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
